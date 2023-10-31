5e7278860719b1330c4a0df1055cd89cb55ad548
Sean W. <cybersholt@gmail.com>
2023-10-31 16:17:08 -0400

    Move from private repo to public

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Sean W. | White Consulting.
+    </title>
+    <link href="/css/main-2.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,400,300,600" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="/js/GlslCanvas.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/84/three.min.js"></script>
+    <script src="/js/lava.js"></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TVRM63S');</script>
+    <!-- End Google Tag Manager -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-70987009-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+    <meta name="p:domain_verify" content="18cdb8dd842b4cbe945ef0edbdc52d27"/>
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVRM63S" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+<div id="" class="content-container">
+    <div id="main">
+        <p>
+            <strong>
+                Sean
+            </strong>
+            is not currently developing new websites
+            <br>
+        </p>
+        <p class="sub">
+            Feel free to contact me with <a href="contact.php">this form</a>
+            <!-- <img src="/img/tr-upwork-logo.png" alt="Top Rated on Upwork with 100% success!" title="Top Rated on Upwork with 100% success!">
+            or I am available on <a href="https://www.upwork.com/freelancers/~0187ff090807338046" target="_blank" rel="nofollow"><img style="width: 100px;vertical-align: middle;" src="/img/upwork-logo-transparent.png" alt="Top Rated on Upwork with 100% success!" title="Top Rated on Upwork with 100% success!"></a>
+            -->
+        </p>
+    </div>
+</div>
+<div id="generative-container">
+</div>
+
+</body>
+<!--
+              ___.                       .__           .__   __
+  ____ ___.__.\_ |__   ___________  _____|  |__   ____ |  |_/  |_
+_/ ___<   |  | | __ \_/ __ \_  __ \/  ___/  |  \ /  _ \|  |\   __\
+\  \___\___  | | \_\ \  ___/|  | \/\___ \|   Y  (  <_> )  |_|  |
+ \___  > ____| |___  /\___  >__|  /____  >___|  /\____/|____/__|
+     \/\/          \/     \/           \/     \/
+-->
+</html>
